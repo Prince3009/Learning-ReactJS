412541f610c08f6dc880661eb1abab07b75e1279
Prince3009 <princechourasia1111@gmail.com>
2024-10-29 00:43:22 +0530

    adding logics to TextForm

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -20,17 +20,34 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleClearClick = () => {
+    const newText = "";
+    setText(newText);
+  }
+
   // Declare a state variable 'text' and a function 'setText' to update it
-  const [text, setText] = useState('Enter your text here');                    
+  const [text, setText] = useState('');                    
   return (
-    <div>
+    <>
+    <div className='container'>
       <h1 className='mb-4'>{props.heading}</h1>
         <div className="mb-3"> 
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         {/* mx-3 is used for vertical margins(left and right). It is a bootstrap class  */}
-        <button className="btn btn-primary mx-3" onClick={handleUppercaseClick}>Convert to UPPERCASE</button>
-        <button className="btn btn-primary mx-3" onClick={handleLowercaseClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUppercaseClick}>Convert to UPPERCASE</button>
+        <button className="btn btn-primary mx-2" onClick={handleLowercaseClick}>Convert to lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear text</button>
+
+        
     </div>
+    <div className="container my-3">
+      <h1>Your text summary</h1>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} minutes read</p>
+      <h2>PREVIEW</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
