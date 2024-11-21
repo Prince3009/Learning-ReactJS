1a13d1881b8a99e9fcbfb2546299ad549f0c4585
Prince3009 <princechourasia1111@gmail.com>
2024-11-22 01:07:21 +0530

    Learning Class Based Component

--- news-app/src/components/News.js
+++ news-app/src/components/News.js
@@ -2,11 +2,36 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      articles : [],
+      loading: false
+    }
+  }
+
+  async componentDidMount(){
+    let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a044473279b8453db9ade7b3f2021bd3";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles, loading: false });
+  }
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsSnap - Top Headlines</h2>
+        <div className='row'>
+        {this.state.articles.map((element)=>{
+          return <div className="className col-md-4"  key={element.url}>
+            <NewsItem title={element.title ? element.title.slice(0,45) : element.title} description={element.description ? element.description.slice(0,88) : element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
+          </div>
+          })}
+        </div>
+        <div className='conatiner d-flex justify-content-between'>
+        <button type="button" class="btn btn-primary" m-10>&larr; Previous</button>                {/* &larr and &rarr used for arrows in previos and next button */}
+        <button type="button" class="btn btn-primary" m-10>Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- news-app/src/components/NewsItem.js
+++ news-app/src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
+    let {title, description, imageUrl, newsUrl} = this.props;
     return (
-      <div>
-        this is a news item component
+      <div className='my-3'>
+        <div className="card" style={{width: '18rem'}}>
+          <img src={!imageUrl ? "https://www.investopedia.com/thmb/7hBaNnM-X-PF2f4uaW_pd7qBGbs=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-2183848670-076b937f91864b9b8b394d0f50ec48f6.jpg" : imageUrl} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title">{title}...</h5>
+              <p className="card-text">{description}...</p>
+              <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
+            </div>
+        </div>
       </div>
     )
   }
